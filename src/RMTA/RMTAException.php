529c50371822a7a2e161ef37914169318409afe8
Gilles Chehade <gilles@poolp.org>
2014-12-08 18:47:13 +0100

    do not redeclare

--- src/RMTA/RMTAException.php
+++ src/RMTA/RMTAException.php
@@ -12,20 +12,4 @@ namespace RMTA;
  * @ignore
  */
 class RMTAException extends \Exception {}
-
-class ClientException extends RMTAException {}
-class ServerException extends RMTAException {}
-class RemoteCallError extends ServerException
-{
-	protected $_details;
-	public function __construct($message = "", $code = 0, Exception $previous = NULL, $details = NULL)
-	{
-		parent::__construct($message, $code, $previous);
-		$this->_details = $details;
-	}
-	public function getDetails()
-	{
-		return $this->_details;
-	}
-}
 ?>
